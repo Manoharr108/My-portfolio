9a9bfbcb36523d1ab5da1cd9f19a2234386b30e9
Manohar R <142599616+Manoharr108@users.noreply.github.com>
2024-03-26 15:21:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function calculate_age(birth_month,birth_day,birth_year)
+{
+    let today_date = new Date();
+    let today_year = today_date.getFullYear();
+    let today_month = today_date.getMonth();
+    let today_day = today_date.getDate();
+    let age = today_year - birth_year;
+
+    if ( today_month < (birth_month - 1))
+    {
+        age--;
+    }
+    if (((birth_month - 1) == today_month) && (today_day < birth_day))
+    {
+        age--;
+    }
+    return age;
+}
+document.getElementById("dob").innerHTML = calculate_age(9,13,2004) + " year old"
